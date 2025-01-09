399ab7b72f07d2524003317c64ba50e82bf890d6
rajsahu17 <rajs8324@gmail.com>
2025-01-09 14:39:35 +0530

    list_commands

--- list_commands.py
+++ list_commands.py
@@ -1,5 +1,19 @@
 #append()
-#length
+def raj_append(input1,input2):
+    for i in input2:
+        input1.append(i)
+        return input1
+
+list1 = [1,2,3,4]
+
+i =[5]
+
+new_list= raj_append(list1,i)
+
+print(new_list)
+
+
+#length()
 
 def len_raj(input1):
     length = 0
